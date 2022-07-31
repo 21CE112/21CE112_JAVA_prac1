3de8ab285257aae945f1cc516e7210fbd8e37285
21CE112 <97016145+21CE112@users.noreply.github.com>
2022-07-31 19:56:24 +0530

    Update Ass10CeaserChiper.java

--- Ass10CeaserChiper.java
+++ Ass10CeaserChiper.java
@@ -5,6 +5,8 @@ public class Ass10CeaserChiper {
      // Encrypts text using a shift of s
      public static StringBuffer encrypt(String text, int s)
      {
+          //Here we can use the concept of Stringbuffer in which the logic is if we take any character’s ASCII value and add the shift into it ,it will give the ASCII value of the required character but we want to continue the loop therefore we will subtract the ASCII value of “A” from it and then we will module that number with 26 because there are 26 total alphabets …and then simply add again 65(ASCII value of “A”) to get the Cipher.
+   
          StringBuffer result= new StringBuffer();
    
          for (int i=0; i<text.length(); i++)
@@ -17,6 +19,8 @@ public class Ass10CeaserChiper {
              }
              else
              {
+                                  // The same we can do for lowercase alphabets by subtracting the ASCII value of “a” from given character.
+
                  char ch = (char)(((int)text.charAt(i) +
                                    s - 97) % 26 + 97);
                  result.append(ch);
@@ -28,6 +32,8 @@ public class Ass10CeaserChiper {
      // Driver code
      public static void main(String[] args)
      {
+          //If we consider whole alphabet as 0 to 25 numbers and 26 again as the alphabet a and continue the loop …then cipher is if we take the alphabet a and want to do its fourth shift then consider a as 0 then 4th shift will return e and the same for all other alphabet.
+        
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the string to encrypt: ");
          String text = sc.nextLine();
